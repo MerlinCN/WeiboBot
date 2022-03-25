2c8dd7d45a55c80a3e440ef67eb3bf652648fea9
Merlin <merlin@merlinblog.cn>
2022-03-26 01:04:50 +0800

    ·忽视刷新界面报错

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setuptools.setup(
     name="WeiboBot",
-    version="0.0.3",
+    version="0.0.4",
     author="Merlin",
     author_email="merlin@merlinblog.cn",
     description="基于微博H5 API开发的机器人框架",
